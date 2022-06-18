a177b0df46916daf923da1d0c1d894d02d3de3ba
git-hulk <hulk.website@gmail.com>
2022-06-19 00:00:31 +0800

    Add badge in README

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -4,4 +4,4 @@ set -e -x
 docker pull quay.io/coreos/etcd
 docker-compose -f ./scripts/etcd-compose.yml up -d
 
-go test $(go list ./...) -race -v -covermode=atomic -coverprofile=coverage.out -p 1
+go test -v ./... -covermode=atomic -coverprofile=coverage.out
