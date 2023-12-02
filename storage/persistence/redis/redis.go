2c6bf9a2458b768f5819de6281f918b72d666179
jihuayu <8042833@qq.com>
2023-12-02 13:43:55 +0800

    update redis code

--- storage/persistence/redis/redis.go
+++ storage/persistence/redis/redis.go
@@ -5,17 +5,15 @@ import (
 	"errors"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/apache/kvrocks-controller/storage/persistence"
 	"github.com/go-redis/redis/v8"
-	"go.etcd.io/etcd/clientv3/concurrency"
+	"go.uber.org/atomic"
 )
 
 const (
-	sessionTTL         = 6
-	defaultDailTimeout = 5 * time.Second
+	sessionTTL = 6 * time.Second
 )
 
 const defaultElectPath = "/kvrocks/controller/leader"
@@ -38,7 +36,7 @@ type Redis struct {
 	isReady   atomic.Bool
 
 	quitCh         chan struct{}
-	electionCh     chan *concurrency.Election
+	wg             sync.WaitGroup
 	leaderChangeCh chan bool
 }
 
@@ -65,11 +63,41 @@ func New(id string, cfg *Config) (*Redis, error) {
 		electPath:      electPath,
 		client:         client,
 		quitCh:         make(chan struct{}),
-		electionCh:     make(chan *concurrency.Election),
 		leaderChangeCh: make(chan bool),
 	}
 	e.isReady.Store(false)
+	e.wg.Add(1)
+	go e.electLoop(context.Background())
 	return e, nil
+}
+
+func (e *Redis) ID() string {
+	return e.myID
+}
+
+func (e *Redis) Leader() string {
+	e.leaderMu.RLock()
+	defer e.leaderMu.RUnlock()
+	return e.leaderID
+}
+
+func (e *Redis) LeaderChange() <-chan bool {
+	return e.leaderChangeCh
+}
+
+func (e *Redis) IsReady(ctx context.Context) bool {
+	for {
+		select {
+		case <-e.quitCh:
+			return false
+		case <-time.After(100 * time.Millisecond):
+			if e.isReady.Load() {
+				return true
+			}
+		case <-ctx.Done():
+			return e.isReady.Load()
+		}
+	}
 }
 
 func (e *Redis) Get(ctx context.Context, key string) ([]byte, error) {
@@ -77,8 +105,7 @@ func (e *Redis) Get(ctx context.Context, key string) ([]byte, error) {
 	if resp.Err() != nil {
 		return nil, resp.Err()
 	}
-
-	return []byte(resp.Val()), nil
+	return resp.Bytes()
 }
 
 func (e *Redis) Exists(ctx context.Context, key string) (bool, error) {
@@ -86,11 +113,11 @@ func (e *Redis) Exists(ctx context.Context, key string) (bool, error) {
 	if resp.Err() != nil {
 		return false, resp.Err()
 	}
-	return resp.Val() > 0, nil
+	return resp.Val() == 1, nil
 }
 
 func (e *Redis) Set(ctx context.Context, key string, value []byte) error {
-	resp := e.client.Set(ctx, key, string(value), 0)
+	resp := e.client.Set(ctx, key, value, 0)
 	if resp.Err() != nil {
 		return resp.Err()
 	}
@@ -106,31 +133,14 @@ func (e *Redis) Delete(ctx context.Context, key string) error {
 }
 
 func (e *Redis) List(ctx context.Context, prefix string) ([]persistence.Entry, error) {
-	entries := make([]persistence.Entry, 0)
-	next, err := e.Scan(ctx, 0, prefix, entries)
-	if err != nil {
-		return entries, err
-	}
-	for next != 0 {
-		next, err = e.Scan(ctx, 0, prefix, entries)
-		if err != nil {
-			return entries, err
-		}
-	}
-	return entries, nil
-}
-
-func (e *Redis) Scan(ctx context.Context, beginCursor uint64, prefix string, entries []persistence.Entry) (uint64, error) {
-	resp := e.client.Scan(ctx, beginCursor, prefix, 20)
+	resp := e.client.Keys(ctx, prefix+"/*")
 	if resp.Err() != nil {
-		return 0, resp.Err()
+		return nil, resp.Err()
 	}
-	keys, cursor := resp.Val()
+	keys := resp.Val()
+	entries := make([]persistence.Entry, 0, len(keys))
 	prefixLen := len(prefix)
 	for _, kv := range keys {
-		if string(kv) == prefix {
-			continue
-		}
 		key := strings.TrimLeft(string(kv[prefixLen+1:]), "/")
 		if strings.ContainsRune(key, '/') {
 			continue
@@ -144,6 +154,46 @@ func (e *Redis) Scan(ctx context.Context, beginCursor uint64, prefix string, ent
 		}
 
 	}
-	return cursor, nil
+	return entries, nil
+}
 
+// we use reids to implement the leader election
+func (e *Redis) electLoop(ctx context.Context) {
+	defer e.wg.Done()
+reset:
+	select {
+	case <-e.quitCh:
+		return
+	default:
+	}
+	res := e.client.SetNX(ctx, e.electPath, e.myID, sessionTTL)
+	if res.Err() != nil {
+		time.Sleep(sessionTTL / 3)
+		goto reset
+	}
+	for {
+		time.Sleep(sessionTTL / 3)
+		resp := e.client.Get(ctx, e.electPath)
+		if resp.Err() != nil {
+			// if the key is not exist or error, goto reset
+			goto reset
+		}
+		if resp.Val() == e.myID {
+			// if the key is set by myself, set ex electPath myId
+			res := e.client.Set(ctx, e.electPath, e.myID, sessionTTL)
+			if res.Err() != nil {
+				goto reset
+			}
+		}
+		select {
+		case <-e.quitCh:
+			return
+		default:
+		}
+	}
+}
+func (e *Redis) Close() error {
+	close(e.quitCh)
+	e.wg.Wait()
+	return e.client.Close()
 }
