0dda5ab90344212420d6d6942531a13c729ab982
Byron Seto <byron.seto@stackadapt.com>
2025-05-12 15:20:13 -0600

    helper to use shard.IsMigrating function

--- cmd/client/command/helper.go
+++ cmd/client/command/helper.go
@@ -51,7 +51,7 @@ func printCluster(cluster *store.Cluster) {
 				role = strings.ToUpper(store.RoleMaster)
 			}
 			migratingStatus := "NO"
-			if shard.MigratingSlot.IsMigrating {
+			if shard.IsMigrating() {
 				migratingStatus = fmt.Sprintf("%s --> %d", shard.MigratingSlot, shard.TargetShardIndex)
 			}
 			columns := []string{fmt.Sprintf("%d", i), node.ID(), node.Addr(), role, migratingStatus}
